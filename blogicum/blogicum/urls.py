edd2f01a3324e8241fb83b3b79c15af466bd16fc
Alina Iv <alina.ivontzyna@yandex.ru>
2025-01-02 17:42:50 +0300

    initial and (hope) final

---
+++ blogicum/blogicum/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import include, path, reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
+urlpatterns = [
+    path('', include('blog.urls')),
+    path('pages/', include('pages.urls')),
+    path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('blog:index'),
+        ),
+        name='registration',
+    ),
+]
+
+handler404 = 'pages.views.page_not_found'
+handler403 = 'pages.views.csrf_failure'
+handler500 = 'pages.views.server_error'
